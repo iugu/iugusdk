f054f74763821188a79c6801e0abfea9479951b0
Patrick Negri <patrick@iugu.com.br>
2012-06-06 10:48:55 -0300

    [ ! ] Fixed OAuth

--- config/initializers/social_accounts.rb
+++ config/initializers/social_accounts.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
     configure do |config|
-      config.path_prefix = "/settings/account/link"
+      config.path_prefix = "/login/using"
     end
     #provider :developer unless Rails.env.production?
     provider :twitter, 'BoAbWcBtO2j3zIVMaQNmg', 'M4n1alFTKQlDQ8geFDXByIDUYfVggde5EDB1PinBs'
