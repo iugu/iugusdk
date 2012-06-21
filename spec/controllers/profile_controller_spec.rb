001c5c466c97549b73a807725ec21066d117be22
alepaez <alepaezseq@gmail.com>
2012-06-21 15:22:31 -0300

    [ Completed #31569549 ] Destroy social tests

--- spec/controllers/profile_controller_spec.rb
+++ spec/controllers/profile_controller_spec.rb
@@ -36,4 +36,66 @@ describe ProfileController do
      end
   
   end
+
+  context "destroy_social" do
+    
+    login_as_user
+
+    before(:each) do
+      @user.social_accounts << Fabricate(:social_account_without_user)
+    end
+
+
+    context "when user has email and password" do
+
+      before do
+        get :destroy_social, :id => @user.social_accounts.first.id
+      end
+
+      it { response.should redirect_to profile_settings_path }
+
+      it 'should notice OK message' do
+        flash.now[:notice].should match I18n.t("iugu.social_unlinked")
+      end
+    
+    end
+
+    context "when user has no email or password" do
+
+      before do
+        @user = Fabricate.build(:user_without_email)
+        @user.social_accounts << Fabricate(:social_account_without_user)
+        @user.skip_confirmation!
+        @user.save(:validate => false)
+        @user.reload
+        sign_in @user
+        get :destroy_social, :id => @user.social_accounts.first.id
+      end
+
+      it { response.should redirect_to profile_settings_path }
+
+      it 'should notice no email/password error' do
+        flash.now[:notice].should match I18n.t("errors.messages.only_social_and_no_email")
+      end
+    
+    end
+
+    context "when social_account id is wrong" do
+
+      before do
+        get :destroy_social, :id => 9128739127
+      end
+
+      it { response.should redirect_to profile_settings_path }
+
+      it 'should notice record not found' do
+        flash.now[:notice].should match I18n.t("errors.messages.not_found")
+      end
+    
+    end
+    
+  
+    
+  
+  end
 end
