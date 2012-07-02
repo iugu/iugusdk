e7b7c0e5eccb61a97c344a95acc6199480646f3f
alepaez <alepaezseq@gmail.com>
2012-07-02 17:51:42 -0300

    [ Completed #32042909 ] InvitationsController now require user authentication

--- app/controllers/iugu/invitations_controller.rb
+++ app/controllers/iugu/invitations_controller.rb
@@ -1,4 +1,4 @@
-class Iugu::InvitationsController < ApplicationController
+class Iugu::InvitationsController < SettingsController
 
   def new
     @user_invitation = UserInvitation.new
@@ -11,6 +11,10 @@ class Iugu::InvitationsController < ApplicationController
 
   def edit
     render :file => "#{Rails.root}/public/404.html", :status => :not_found unless @user_invitation = UserInvitation.find_by_invitation_token(params[:invitation_token])
+  end
+
+  def update
+    
   end
 
 end
