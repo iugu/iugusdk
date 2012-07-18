6303411d0afc8a5baae642799558d2a5cefaa92a
alepaez <alepaezseq@gmail.com>
2012-07-18 17:23:58 -0300

    [ Completed #32740501 ] Account has many account domains

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Account do
 
+  it { should have_many(:account_domains) }
   it { should have_many(:account_users) }
   it { should have_many(:users).through(:account_users) }
 
