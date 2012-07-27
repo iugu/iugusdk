6de054ecd88fc0be9cbc14112ea587cc8a08372f
alepaez <alepaezseq@gmail.com>
2012-07-27 19:22:53 -0300

    [ Completed #33496731 ] AccountUsersController is now using must_be filter

--- app/controllers/iugu/account_users_controller.rb
+++ app/controllers/iugu/account_users_controller.rb
@@ -1,5 +1,7 @@
 class Iugu::AccountUsersController < Iugu::AccountSettingsController
 
+  before_filter(:only => [:destroy, :cancel_destruction]) { |c| c.must_be [:owner, :admin], :account_id }
+
   def index
     @account = Account.find(params[:account_id])
     @account_users = @account.account_users
@@ -17,24 +19,16 @@ class Iugu::AccountUsersController < Iugu::AccountSettingsController
     else
       raise ActionController::RoutingError.new('Not Found')
     end
-    if current_user.is?(:owner, @account) || current_user.is?(:owner, @account)
-      raise ActionController::RoutingError.new('Access Denied') if @account_user.user_id == current_user.id || @account_user.is?(:owner)
-      @account_user.destroy
-    else
-      raise ActionController::RoutingError.new('Access Denied')
-    end
+    raise ActionController::RoutingError.new('Access Denied') if @account_user.user_id == current_user.id || @account_user.is?(:owner)
+    @account_user.destroy
     redirect_to account_users_index_path(params[:account_id]), :notice => I18n.t("iugu.account_user_destruction_in") + @account_user.destruction_job.run_at.to_s
   end
 
   def cancel_destruction
     if @account_user = AccountUser.find_by_account_id_and_user_id(params[:account_id], params[:user_id])
       @account = Account.find(params[:account_id])
-      if current_user.is?(:owner, @account) || current_user.is?(:owner, @account) 
-        @account_user.cancel_destruction
-        redirect_to account_users_index_path(params[:account_id]), :notice => I18n.t("iugu.account_user_destruction_undone")
-      else
-        raise ActionController::RoutingError.new('Access Denied')
-      end
+      @account_user.cancel_destruction
+      redirect_to account_users_index_path(params[:account_id]), :notice => I18n.t("iugu.account_user_destruction_undone")
     else
       raise ActionController::RoutingError.new('Not Found')
     end
