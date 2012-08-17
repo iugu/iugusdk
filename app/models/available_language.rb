daf0eb0cb108bf36467755c8d82ef289a0c55a76
Patrick Negri <patrick@iugu.com.br>
2012-08-17 17:21:08 -0300

    [ ! ] Fixing Locale ComboBox Inconsistency

--- app/models/available_language.rb
+++ app/models/available_language.rb
@@ -1,9 +1,11 @@
+# encoding: utf-8
+#
 class AvailableLanguage
   def self.all
     begin
       YAML.load_file("#{Rails.root.to_s}/config/available_language.yml")['locales']
     rescue
-      {"English" => "en", 'Portugues(Brasil)' => "pt-BR"}
+      {"English" => "en", 'Português (Brasil)' => "pt-BR"}
     end
   end
   
--- spec/dummy/config/initializers/iugusdk.rb
+++ spec/dummy/config/initializers/iugusdk.rb
@@ -1,4 +1,5 @@
 IuguSDK.setup do |config|
   config.app_main_url = '/'
   config.application_title = 'Dummy'
+  config.enable_social_login = true
 end
