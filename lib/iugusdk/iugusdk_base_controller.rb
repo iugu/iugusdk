7856920efd19e878008f634130f5c4ea82a113db
Alexandre Paez <alepaezseq@gmail.com>
2012-10-31 10:01:28 -0200

    [ Completed #38677803 ] Setting locale with params hl

--- lib/iugusdk/iugusdk_base_controller.rb
+++ lib/iugusdk/iugusdk_base_controller.rb
@@ -9,15 +9,19 @@ module IuguSDKBaseController
   end
 
   def configure_locale
-    @matched_locale_from_browser = request.preferred_language_from(AvailableLanguage.all.values)
-    if signed_in?
-      if current_user.locale.blank?
-        locale = "en" 
-      else
-        locale = current_user.locale
-      end
+    if(params[:hl])
+      locale = params[:hl] if AvailableLanguage.all.values.include? params[:hl]
     else
-      locale = @matched_locale_from_browser
+      @matched_locale_from_browser = request.preferred_language_from(AvailableLanguage.all.values)
+      if signed_in?
+        if current_user.locale.blank?
+          locale = "en" 
+        else
+          locale = current_user.locale
+        end
+      else
+        locale = @matched_locale_from_browser
+      end
     end
     I18n.locale = locale
   end
